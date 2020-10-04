f01be1e46f57a47699ffaf5a504a7de139ce50d1
zakuro <z@kuro.red>
2020-10-04 14:44:34 +0900

    opt

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+extern crate getopts;
+use getopts::Options;
+use std::env;
+
+fn print_help(program: &str, opts: Options) {
+    let brief = format!("Usage: {} FILE [options]", program);
+    print!("{}", opts.usage(&brief));
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let program = args[0].clone();
+    let  mut opts = Options::new();
+    opts.optflag("h", "help", "Print help");
+    let matches = match opts.parse(&args[1..]) {
+        Ok(m) => { m }
+        Err(f) => { panic!(f.to_string()) }
+    };
+
+    if matches.opt_present("h") {
+        print_help(&program, opts);
+        return;
+    }
+    println!("Fenixcc");
 }
