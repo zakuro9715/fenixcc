8083e65d5e75094d26dd22f3027f290a1dc80e5d
zakuro <z@kuro.red>
2020-10-04 14:58:22 +0900

    Refacotr

--- src/main.rs
+++ src/main.rs
@@ -7,19 +7,26 @@ fn print_help(program: &str, opts: Options) {
     print!("{}", opts.usage(&brief));
 }
 
+fn setup_opts(opts: &mut Options) {
+    opts.optflag("h", "help", "Print help");
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     let program = args[0].clone();
-    let  mut opts = Options::new();
-    opts.optflag("h", "help", "Print help");
+
+    let mut opts = Options::new();
+    setup_opts(&mut opts);
+
     let matches = match opts.parse(&args[1..]) {
-        Ok(m) => { m }
-        Err(f) => { panic!(f.to_string()) }
+        Ok(m) => m,
+        Err(f) => panic!(f.to_string()),
     };
 
     if matches.opt_present("h") {
         print_help(&program, opts);
         return;
     }
+
     println!("Fenixcc");
 }
