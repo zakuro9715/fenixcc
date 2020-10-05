bf171969952dd487b648fa55f9ce87a23cc76738
zakuro <z@kuro.red>
2020-10-06 03:27:23 +0900

    Add ast

---
+++ src/ast.rs
@@ -0,0 +1,37 @@
+use crate::token::{Token, TokenKind, Pos};
+
+#[derive(Debug, Clone)]
+pub struct AST {
+    token: Token,
+    node: Node,
+}
+
+#[derive(Debug, Clone)]
+pub enum Node {
+    Addition(Box<AST>, Box<AST>),
+    Subtraction(Box<AST>, Box<AST>),
+    Literal,
+}
+
+impl AST {
+    pub fn new(token: Token, node: Node) -> Box<AST> {
+        Box::new(AST { token, node })
+    }
+
+    pub fn new_literal(token: Token) -> Box<AST> {
+        debug_assert!(token.kind.is_literal());
+        Box::new(AST { token, node: Node::Literal })
+    }
+}
+
+#[test]
+fn test_new_literal() {
+    AST::new_literal(Token::new("0".to_string(), TokenKind::Int(0), Pos::head()));
+}
+
+#[test]
+#[should_panic]
+fn test_new_literal_invalid() {
+    AST::new_literal(Token::new("".to_string(), TokenKind::EOF, Pos::head()));
+}
+
