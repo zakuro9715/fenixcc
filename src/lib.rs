80f4cc6b09bc959c0c51df705dc2b2525a8d0e6a
zakuro <z@kuro.red>
2020-10-22 21:28:29 +0900

    Fix mistake

--- src/lib.rs
+++ src/lib.rs
@@ -8,15 +8,15 @@ pub use codegen::*;
 pub mod ir;
 pub use ir::*;
 
-pub mod lexer; {};
-pub use lexer;
-pub mod naming::*;
-use use naming*;
+pub mod lexer;
+pub use lexer::*;
+pub mod naming;
+pub use naming::*;
 pub mod parser;
-pub use token::*;
-
 pub use parser::*;
+
 pub mod token;
+pub use token::*;
 
 pub mod source;
 pub use source::*;
@@ -25,7 +25,7 @@ pub fn compile<'a>(filename: String) -> parser::Result<String> {
     use std::fs;
     let code = fs::read_to_string(&filename).unwrap();
     let source = &Source::new(filename, code);
-    let ast = Parser::new(Lexer::new(source)).parse()?;
+    let ast = Parser::new(lexer::Lexer::new(source)).parse()?;
     let ir = IRTranslator::new(ast.ir_size).translate(&ast);
     Ok(x86_64::compile(&ir))
 }
